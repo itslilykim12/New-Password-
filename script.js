b5ed0a75657e783e47c9a311f67a3d7a8722440b
nlilu <nlilum.kim@gmail.com>
2021-03-14 11:45:59 -0400

    final touches to javascript

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 // Assignment Code]
-const passwordDisplay = document.getElementById('password');
-const symbols = document.getElementById('symbols');
-const generateBtn = document.getElementById('generate');
-const length = document.getElementById('length');
-const numbers = document.getElementById('numbers');
-const uppercase = document.getElementById('uppercase');
-const lowercase = document.getElementById('lowercase');
+var generate = document.querySelector('generate')
+var inputLength = document.querySelector('input[name="length"]');
+var num = document.querySelector('input[name="number"]');
+var sym = document.querySelector('input[name="symbol"]');
+var upper = document.querySelector('input[name="uppercase"]')
+var lower = document.querySelector('input[name="lowercase"]')
 
 const passKeys = {
-    lowercase: 'abcdefghijklmnopqrstuvwxyz',
-    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    number: '0123456789',
-    symbol: '*!@#$%^&<>?'
-}
-
-// Write password to the #password input
-function generatePassword(lowercase, uppercase, number, symbol, length) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  number: '0123456789',
+  symbol: '*!@#$%^&<>?',
+ };
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", e => {
-  e.preventDefault()
-  const length = length.value
-  const includeSymbols = includeSymbolsElement.checked
-  const includeNums = numbers.checked
-  const includeUpper = uppercase.checked
-  const includerLower = lowercase.checked
-  console.log(length,includeSymbols, includeNums, includeUpper, includerLower)
-  const password = generatePassword(length, includeSymbols, includeNums, includeUpper, includerLower);
-  passwordText.value = password;
+generate.addEventListener("click", () => {
+  var length = +inputLength.value; 
+  var activeLower = lower.checked; 
+  var activeUpper = upper.checked; 
+  var activeNumber = num.checked; 
+  var activeSymbol = sym.checked; 
+
+  generatePassword(activeLower, activeUpper, activeNumber, activeSymbol, length);
 }); 
+ 
+  
+// Write password to the #password input
+function generatePassword(lower, upper, num, sym, length) {
+  let main = ""; 
+  let finalPassword = ""; 
 
+  const passOptions = {
+    lowercase: lower, 
+    uppercase: upper, 
+    number: num, 
+    symbol: sym,
+  };
 
-
-
+  for (i=0;i<Object.keys(passOptions).length; i++) {
+    main += (object.values(passOptions)[i]) ? passKeys[Object.keys(passOptions)[i]] : "";
+  }
+  if (main = ! "" && length > 0){
+    for (i=0;i<length;i++){
+      finalPassword +=main[Math.floor(Math.random()* main.length)];
+    }
+  }     
+}
